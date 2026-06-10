bb37ac868ec172ef84aedb63ad0889a72ba5f240
Evgeniy Vorobyov <evgeniyvorobyov01071992@gmail.com>
2026-06-10 13:26:45 +0300

    fix password auth syntax

--- auth.py
+++ auth.py
@@ -1,1 +1,12 @@
-import hashlib, os`n`ndef hash_password(password, salt=None):`n    if salt is None: salt = os.urandom(16).hex()`n    salted = password + salt`n    return hashlib.sha256(salted.encode()).hexdigest(), salt`n`ndef verify_password(input_password, db_hash, db_salt):`n    input_hash, _ = hash_password(input_password, db_salt)`n    return input_hash == db_hash
+import hashlib
+import os
+
+def hash_password(password, salt=None):
+    if salt is None:
+        salt = os.urandom(16).hex()
+    salted = password + salt
+    return hashlib.sha256(salted.encode()).hexdigest(), salt
+
+def verify_password(input_password, db_hash, db_salt):
+    input_hash, _ = hash_password(input_password, db_salt)
+    return input_hash == db_hash
